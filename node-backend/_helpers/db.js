ba6f1f6e1d577462a20d6bf51ae77a90aac9ec8f
John Briggs <jjhbriggs@gmail.com>
2021-12-14 19:21:48 -0500

    pg update

--- node-backend/_helpers/db.js
+++ node-backend/_helpers/db.js
@@ -1,5 +1,5 @@
 const config = require('config.json');
-const mysql = require('mysql2/promise');
+const pgp = require('pg-promise')();
 const { Sequelize } = require('sequelize');
 
 module.exports = db = {};
@@ -9,11 +9,14 @@ initialize();
 async function initialize() {
     // create db if it doesn't already exist
     const { host, port, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host, port, user, password });
-    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+    //const connection = await mysql.createConnection({ host, port, user, password });
+
+    const connection = pgp(config.database);
+
+    //await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, { dialect: 'postgres' });
 
     // init models and add them to the exported db object
     db.User = require('../users/user.model')(sequelize);
